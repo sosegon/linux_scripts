e91b05abd5e916c3c939f7060835afad653fd69e
Sebastian <anse23@hotmail.com>
2017-03-01 23:32:59 -0500

    feat: Finished logic to classify images

--- manual_image_classifier/main.py
+++ manual_image_classifier/main.py
@@ -2,7 +2,16 @@ import sys
 import os
 import glob
 import main_window
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
+
+from skimage import io
+import shutil
+
+# from http://stackoverflow.com/a/7376838/1065981
+try:
+    from PyQt4.QtCore import QString
+except ImportError:
+    QString = str
 
 class ImageScene(QtGui.QGraphicsScene):
     def __init__(self, parent):
@@ -14,14 +23,21 @@ class ImageScene(QtGui.QGraphicsScene):
             item = self.items()[-1]
             self.removeItem(item)
 
-class ImageClassifier(QtGui.QMainWindow, main_window.Ui_Dialog):
+class ImageClassifier(QtGui.QMainWindow, main_window.Ui_MainWindow):
     def __init__(self):
         
         super(self.__class__, self).__init__()
         self.setupUi(self)
         self.image_extensions = ["png", "jpg", "ppm"]
         self.src_folder_path, self.dst_folder_path = "", ""
+        self.valid_images_folder_path = "" # to store classified images
+        self.discarded_images_folder_path = "" # to store classified images
+        self.classes_file_name = "" # csv with paths to images and classes
         self.image_paths = []
+        self.isClassifying = False
+        self.classified_count = 0
+        self.discarded_count = 0
+        self.current_file_name = ""
 
         self.scene = ImageScene(self)
 
@@ -48,30 +64,106 @@ class ImageClassifier(QtGui.QMainWindow, main_window.Ui_Dialog):
         if(self.src_folder_path == "" or self.dst_folder_path == ""):
             return
 
-        self.image_paths = self.get_files(self.src_folder_path, True)
+        self.image_paths = self.get_file_paths(self.src_folder_path, True)
 
-        for im_path in image_paths:
+        if len(self.image_paths) == 0:
+            self.statusbar.showMessage(QString("No images to classify"))
+
+        # Create folder for images
+        self.valid_images_folder_path = self.dst_folder_path + "/valid_images"
+        self.discarded_images_folder_path = self.dst_folder_path + "/discarded_images"
+        os.makedirs(self.valid_images_folder_path)
+        os.makedirs(self.discarded_images_folder_path)
+
+        # Create csv file with paths and classes
+        self.classes_file_name = self.dst_folder_path + "/data.csv"
+        with open(self.classes_file_name, "w+") as f:
+            try:
+                f.writelines("path, class")
+            finally:
+                f.close()
+
+        self.txt_class_name.setEnabled(True)
+        self.txt_class_name.setFocus()
+        self.isClassifying = True
+
+        self.current_file_name = self.image_paths[self.classified_count + self.discarded_count]
+        self.open_image(self.current_file_name)
+    
+    def keyPressEvent(self, event):
+        if self.isClassifying:
+            file_name = self.get_file_name(self.current_file_name)
+            if event.key() == QtCore.Qt.Key_Enter:
+                full_new_name = self.valid_images_folder_path + "/" + file_name
+                shutil.copyfile(self.current_file_name, full_new_name)
+
+                relative_path = "./images/" + file_name
+                clazz = str(self.txt_class_name.text()).lower().strip()
+                with open(self.classes_file_name, "a") as f:
+                    try:
+                        f.writelines("\n" + relative_path + ", " + clazz)
+                    finally:
+                        f.close()
+                self.classified_count += 1
+                remaining = len(self.image_paths) - self.discarded_count - self.classified_count
+                self.statusbar.showMessage(QString("Remaining images: " + str(remaining) + ", " + file_name + " classified as " + clazz))
+            elif event.key() == QtCore.Qt.Key_Escape:
+                full_new_name = self.discarded_images_folder_path + "/" + file_name
+                shutil.copyfile(self.current_file_name, full_new_name)
+                self.discarded_count += 1
+                remaining = len(self.image_paths) - self.discarded_count - self.classified_count
+                self.statusbar.showMessage(QString("Remaining images: " + str(remaining) + ", " + file_name + " discarded"))
+
+            self.txt_class_name.setFocus()
+            self.txt_class_name.clear()
+            total_count = self.classified_count + self.discarded_count
             
+            if total_count >= len(self.image_paths):
+                self.txt_class_name.setEnabled(False)
+                self.statusbar.showMessage(QString("Files classified: " + str(self.classified_count) + ", Files discarded: " + str(self.discarded_count)))
+                self.isClassifying = False
 
-    def get_files(self, dir_path, recursive):
+            self.current_file_name = self.image_paths[total_count]
+            self.open_image(self.current_file_name)
+
+    def open_image(self, file_name):
+        self.scene.reset()
+
+        pxm_image = QtGui.QPixmap(file_name)
+        self.scene.addPixmap(pxm_image)
+        self.gv_image.setScene(self.scene)
+
+        self.image_file_name = str(file_name)
+        self.image_data_array = io.imread(self.image_file_name)
+
+    def get_file_paths(self, dir_path, recursive):
         file_names = []
         for fname in glob.glob(dir_path + "/**/*", recursive=recursive):
-            print(fname)
             if self.is_image_path(fname):
                 file_names.append(fname)
 
         return file_names
 
-
     def is_image_path(self, file_name):
+        return self.get_file_extension(file_name) != -1
+
+    def get_file_extension(self, file_name):
         reverse = file_name[::-1]
         try:
             dot_index = reverse.index(".")
             extension = reverse[:dot_index][::-1].lower().strip()
-            return extension in self.image_extensions
+            return extension
         except:
-            return False
+            return -1
 
+    def get_file_name(self, file_name):
+        reverse = file_name[::-1]
+        try:
+            dot_index = reverse.index("/")
+            extension = reverse[:dot_index][::-1].lower().strip()
+            return extension
+        except:
+            return -1
 
 def main():
     app = QtGui.QApplication(sys.argv)  # A new instance of QApplication
