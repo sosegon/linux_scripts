7d50d2766a63ba802d4925f16ae5a3e7bb318050
Sebastian <anse23@hotmail.com>
2018-04-12 16:53:27 +0100

    feat: Updated start config file to load partitions

--- start_config_linux_session.sh
+++ start_config_linux_session.sh
@@ -1,13 +1,35 @@
 #!/usr/bin/bash
 
 ## The objective of this script is to set the keyboard to latam spanish,
-## set the slash key to be close to the index finger, and alias git command.
+## set the slash key to be close to the index finger, alias git command, 
+## and load the partitions from the disk
 ## All this aimed to increase speed when typing.
 
+# privilegies
+sudo chown sebastian:sebastian /mnt
+echo "set privilegies for mnt"
+
+# mount partition
+sudo mount -t ext4 /dev/sda2 /mnt/linux_shared
+echo "mount shared partition"
+
+# mount lubuntu partition
+sudo mount -t ext4 /dev/sda4 /mnt/mint
+echo "mount mint partition"
+
+# set variables
+source /mnt/linux_shared/shared/.bashrc
+echo "set variables"
+
+# set keyboard
 setxkbmap -layout 'es,es'
 echo "set spanish keyboard"
-xmodmap ~/.Xmodmap.try
-echo "keys set"
+
+# This is giving some problems, the computer freezes.
+# set keys
+#xmodmap ~/.Xmodmap.try
+#echo "keys set"
+
+# alias for git
 alias gt="git"
 echo "aliased git"
-
