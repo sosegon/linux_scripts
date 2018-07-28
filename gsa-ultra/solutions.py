dbb40c3e5f2e9503d66e3cf5a3921ab980c939b3
Sebastian <anse23@hotmail.com>
2018-07-28 21:53:08 +0100

    feat: Solved down 3

--- gsa-ultra/solutions.py
+++ gsa-ultra/solutions.py
@@ -48,8 +48,104 @@ def down2(s):
 
     return n + 10000
 
-print(down2("RBYYYRRBYYYRBBBRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYRBBRBYYYRBBBBBRYYRYRYRBBRYBRYBBBRYRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBYRYRYRBRYBYYBRBYYYRBBRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBBRBYYYRBBBBBRYYRYRYRBRYBYYBBRRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBRBRYBBRYBRYBRYBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRBRYBYBBRBRBRBRYBBRYBRYBRYBBRYYRRBYYYRRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYYBBRYBRYBRYBBBBRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYRBYRYBRYBRYBYRYRYRBRYBYYBRBYYYRBBRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBBRBYYYRBRYBBBBBBRYYRYRYRBRYBYYBBRRBYYBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBRBRYBBRYBRYBRYBBRYYRYRYRBRYBYYRBBRBRBRYBBRYBRYBRYBBRBRBRYBBRYBYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBRBRYBBRYBRYBRYBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBRYYRRBYYYRRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBRYBRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYB"))
-# print(down2("RBYYYRRBYYYRBBBRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYRBBRBYYYRBBBBBRYYRYRYRBBRYBRYBBBRYRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBR"))
+#print(down2("RBYYYRRBYYYRBBBRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYRBBRBYYYRBBBBBRYYRYRYRBBRYBRYBBBRYRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBYRYRYRBRYBYYBRBYYYRBBRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBBRBYYYRBBBBBRYYRYRYRBRYBYYBBRRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBRBRYBBRYBRYBRYBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRBRYBYBBRBRBRBRYBBRYBRYBRYBBRYYRRBYYYRRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYYBBRYBRYBRYBBBBRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYRBYRYBRYBRYBYRYRYRBRYBYYBRBYYYRBBRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBBRBYYYRBRYBBBBBBRYYRYRYRBRYBYYBBRRBYYBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBRBRYBBRYBRYBRYBBRYYRYRYRBRYBYYRBBRBRBRYBBRYBRYBRYBBRBRBRYBBRYBYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBRBRYBBRYBRYBRYBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBRYYRRBYYYRRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYBBRYBRBYYYRBBBBBRYYRYRYRBRYBYYBBRBRBRYBBRYBRYBRYB"))
+
+def down3(s):
+    assert len(s) >= 4
+
+    l = len(s)
+    index = 2
+    s_left = s[:index]
+    s_right = s[index:]
+
+    costs = []
+
+    while index <= l - 2:
+        l1 = len(s_left)
+        index1 = 1
+        s_left_left = s_left[:index1]
+        s_left_right = s_left[index1:]
+
+        while index1 <= l1 - 1:
+            a = longest_palindrome(s_left_left)
+            b = longest_palindrome(s_left_right)
+            l2 = len(s_right)
+            index2 = 1
+            s_right_left = s_right[:index2]
+            s_right_right = s_right[index2:]
+
+            while index2 <= l2 - 1:
+                c = longest_palindrome(s_right_left)
+                d = longest_palindrome(s_right_right)
+                # print(d)
+                # print(s_left_left + "|" + s_left_right + "|" + s_right_left + "|" + s_right_right)
+                cos = a + b + c + d
+                costs.append(cos)
+                # print(cos)
+                #print("\n")
+
+                index2 = index2 + 1
+                s_right_left = s_right[:index2]
+                s_right_right = s_right[index2:]
+
+            index1 = index1 + 1
+            s_left_left = s_left[:index1]
+            s_left_right = s_left[index1:]
+
+        index = index + 1
+        s_left = s[:index]
+        s_right = s[index:]
+
+    costs_sorted = sorted(costs)
+    return costs_sorted[0]
+
+def longest_palindrome(s):
+    la = s.count("a")
+    lb = s.count("b")
+    lc = s.count("c")
+    ld = s.count("d")
+    le = s.count("e")
+    lf = s.count("f")
+    lg = s.count("g")
+
+    l = []
+    if la > 0:
+        l.append(la)
+    if lb > 0:
+        l.append(lb)
+    if lc > 0:
+        l.append(lc)
+    if ld > 0:
+        l.append(ld)
+    if le > 0:
+        l.append(le)
+    if lf > 0:
+        l.append(lf)
+    if lg > 0:
+        l.append(lg)
+
+    if len(set(l)) == 1 and l[0] == 1:
+        return 1
+
+    l = [la, lb, lc, ld, le, lf, lg]
+    s = sorted(l, reverse=True)
+    highest = s[0]
+
+    s = s[1:]
+    h = []
+    for e in s:
+        if e == 1:
+            h.append(e)
+        else:
+            h.append(e - e % 2)
+
+    return highest + sum(h)
+
+print(down3("abccaa"))
+
+r = down3("aabbbbbbbaabbaaaabbbabbbaaabbabbaaaaabbaaaaababbaaaaabaabbbaabaaabaababbbaaaaaabaaabbaaababaababbbaabbbbaabbbaaaaaaaaababbbbbaabbbabbabababbaaabaabababbbaabbbaabaaabbbaabbbabbbabbabaabbabababbbabbaabb")
+print(r)
+
 
 
 # def across7():
