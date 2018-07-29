0dab20f032939702ace48698ac4f5ab650e7209d
Sebastian <anse23@hotmail.com>
2018-07-29 17:25:50 +0100

    feat: Added unresponsive version of down8
    Need to replace the matrix I.

--- gsa-ultra/solutions.py
+++ gsa-ultra/solutions.py
@@ -1,3 +1,162 @@
+def down8(n, m, r, c, k):
+    w = sorted([x - c[i - 1] for i, x in enumerate(c)][1:]) # widhts
+    h = sorted([x - r[i - 1] for i, x in enumerate(r)][1:]) # heights
+
+    N = 1
+    I = [[0]*(m-1) for i in range(n-1)]
+    I[0][0] = 1
+    pending = []
+    areas = []
+    areas.append(w[0] * h[0])
+    i = 0
+    j = 0
+
+    assert k <= (m-1)*(n-1)
+
+    while N < k or (j < len(I) - 1 or i < len(I[0]) - 1):
+        left = None
+        right = None
+        hasLeft = j < len(I) - 1 and i < len(I[0])
+        hasRight = j < len(I) and i < len(I[0]) - 1
+
+        if hasLeft :
+            if I[j+1][i] == 1: # left already visited
+                j = j + 1
+                continue
+
+        if hasRight:
+            if I[j][i+1] == 1: # right already visited
+                i = i + 1
+                continue
+
+        left_right = []
+        if hasRight:
+            right = w[i+1] * h[j]
+            I[j][i+1] = 1
+            left_right.append(right)
+
+        if hasLeft:
+            left = w[i] * h[j+1]
+            I[j+1][i] = 1
+            left_right.append(left)
+
+        if len(pending) > 0:
+            last = pending[-1]
+            if len(left_right) == 0:
+                # remove as needed
+                while N < k:
+                    pending = pending[:-1]
+                    print("\nremove")
+                    print(pending)
+                    areas.append(last[0])
+
+                    N = N + 1
+                    if len(pending) > 0:
+                        last = pending[-1]
+                    else:
+                        break
+
+            else:
+                while last[0] < min(left_right) and last[0] < max(left_right) and N < k:
+                    il = last[1] + 1
+                    jl = last[2]
+                    if jl < len(I) and il < len(I[0]):
+                        if I[jl][il] == 0:
+                            area = w[il] * h[jl]
+                            pending = add_pending(pending, (area, il, jl))
+                            print("\nsub right")
+                            print(pending)
+                            I[jl][il] = 1
+
+                    il = last[1]
+                    jl = last[2] + 1
+                    if jl < len(I) and il < len(I[0]):
+                        if I[jl][il] == 0:
+                            area = w[il] * h[jl]
+                            pending = add_pending(pending, (area, il, jl))
+                            print("\nsub left")
+                            print(pending)
+                            I[jl][il] = 1
+
+                    pending = pending[:-1]
+                    print("\nremove")
+                    print(pending)
+                    areas.append(last[0])
+
+                    N = N + 1
+                    if len(pending) > 0:
+                        last = pending[-1]
+                    else:
+                        break
+
+        print("break: " + str(N) + ", " + str(k))
+        if N >= k:
+            break
+
+        if hasLeft and hasRight:
+            if right <= left:
+                pending = add_pending(pending, (left, i, j+1))
+                print("\nleft")
+                print(pending)
+                i = i + 1
+                areas.append(right)
+            else:
+                pending = add_pending(pending, (right, i+1, j))
+                print("\nright")
+                print(pending)
+                j = j + 1
+                areas.append(left)
+            N = N + 1
+        elif hasLeft:
+            pending = add_pending(pending, (left, i, j+1))
+            print("\nleft")
+            print(pending)
+            j = j + 1
+        elif hasRight:
+            pending = add_pending(pending, (right, i+1, j))
+            print("\nright")
+            print(pending)
+            i = i + 1
+
+    return areas
+
+def add_pending(listed, elem):
+    inserted = False
+
+    if len(listed) == 0:
+        listed.append(elem)
+    else:
+        index = 0
+        while index < len(listed):
+            if listed[index][0] > elem[0]:
+                index = index + 1
+            else:
+                break
+
+        listed = listed[:index] + [elem] + listed[index:]
+
+    return listed
+
+# a = down8(6, 6, (1,7,12,15,21,23), (1,7,11,14,24,31), 5)
+# print(a)
+
+f = open("down8.txt", "r")
+v = f.read().split("\n")
+dim = v[0].split()
+n = int(dim[0])
+m = int(dim[1])
+k = int(v[3])
+r = tuple(map(int, v[1].split()))
+c = tuple(map(int, v[2].split()))
+
+
+
+a = down8(n, m, r, c, 5)
+print(a)
+
+
+
+
 def across12(n, x, r):
     assert n == len(x) and n == len(r)
 
