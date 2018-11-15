db822e507b49d9cf22b9769bdd73411cf4d9cdeb
Sebastian <anse23@hotmail.com>
2018-11-15 14:35:45 -0500

    feat: Modified script to use password automatically

--- start_config_linux_session.sh
+++ start_config_linux_session.sh
@@ -1,20 +1,22 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 ## The objective of this script is to set the keyboard to latam spanish,
-## set the slash key to be close to the index finger, alias git command, 
+## set the slash key to be close to the index finger, alias git command,
 ## and load the partitions from the disk
 ## All this aimed to increase speed when typing.
 
+# Change your password in 'your_pasword'
+
 # privilegies
-sudo chown sebastian:sebastian /mnt
+echo 'your_password' | sudo -S chown sebastian:sebastian /mnt
 echo "set privilegies for mnt"
 
 # mount partition
-sudo mount -t ext4 /dev/sda2 /mnt/linux_shared
+echo 'your_password' | sudo -S mount -t ext4 /dev/sda2 /mnt/linux_shared
 echo "mount shared partition"
 
 # mount lubuntu partition
-sudo mount -t ext4 /dev/sda4 /mnt/mint
+echo 'your_password' | sudo -S mount -t ext4 /dev/sda4 /mnt/mint
 echo "mount mint partition"
 
 # set variables
@@ -24,11 +26,6 @@ echo "set variables"
 # set keyboard
 setxkbmap -layout 'es,es'
 echo "set spanish keyboard"
-
-# This is giving some problems, the computer freezes.
-# set keys
-#xmodmap ~/.Xmodmap.try
-#echo "keys set"
 
 # alias for git
 alias gt="git"
