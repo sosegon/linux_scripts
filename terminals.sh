c62213ca76be3af45a74f419ac29ad138c2418a1
Sebastian <anse23@hotmail.com>
2018-11-15 15:54:48 -0500

    feat: Added arguments to terminals script

--- terminals.sh
+++ terminals.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 # This config is for a 17" screen with a top and bottom panels in lubuntu
-
-xterm -hold -geometry 95x25+0+0 -fa 'Monospace' -fs 12 & # top-letf
-xterm -hold -geometry 96x25+955+26 -fa 'Monospace' -fs 12  & # top-right
-xterm -hold -geometry 95x26+0+533 -fa 'Monospace' -fs 12  & # bottom-left
-xterm -hold -geometry 96x26+955+533 -fa 'Monospace' -fs 12 # bottom-right
+if [ $1 -eq 1 ]
+then
+	xterm -hold -geometry 95x25+0+0 -fa 'Monospace' -fs 12 & # top-letf
+elif [ $1 -eq 2 ]
+then
+	xterm -hold -geometry 96x25+955+26 -fa 'Monospace' -fs 12  & # top-right
+elif [ $1 -eq 3 ]
+then
+	xterm -hold -geometry 95x26+0+533 -fa 'Monospace' -fs 12  & # bottom-left
+elif [ $1 -eq 4 ]
+then
+	xterm -hold -geometry 96x26+955+533 -fa 'Monospace' -fs 12 # bottom-right
+else
+	xterm -hold -geometry 95x25+0+0 -fa 'Monospace' -fs 12 & # top-letf
+	xterm -hold -geometry 96x25+955+26 -fa 'Monospace' -fs 12  & # top-right
+	xterm -hold -geometry 95x26+0+533 -fa 'Monospace' -fs 12  & # bottom-left
+	xterm -hold -geometry 96x26+955+533 -fa 'Monospace' -fs 12 # bottom-right
+fi
